72ca598c63254555f94070646fbf9643ae6ba1b2
DACOSTA96 <diego.ac9614@gmail.com>
2024-03-15 16:04:08 -0500

    v8 implementacion estados en el buscador

--- src/ComponenteBuscar.js
+++ src/ComponenteBuscar.js
@@ -1,12 +1,16 @@
-
+import React from 'react';
 import "./estilos.css"
 
 function ComponenteBuscar(){
+    const[textoBuscador,settextoBuscador]= React.useState("")
+
+    console.log(textoBuscador)
+
     return(
         <div className="buscador"> 
             <input 
                 onChange={(event)=>{
-                    console.log(event.target.value)
+                   settextoBuscador(event.target.value)
                 }}
                  type="text" 
                  placeholder="Buscar tarea" 
