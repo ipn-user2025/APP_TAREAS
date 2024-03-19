ea4660ca9040899b2aecae5158f4532ac742f143
DACOSTA96 <diego.ac9614@gmail.com>
2024-03-19 11:08:36 -0500

    v10 implementacion estados de carga

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { ComponenteLista } from './componentes/ComponenteLista';
 import { ComponenteBoton } from './componentes/ComponenteBoton';
 import {ComponenteTituloPrincipal} from './componentes/ComponenteTituloPrincipal';
 import img1 from './img/img inicio.png';
+import imgLogo from './img/loading-loader.gif';
 
 
 // const tareasDefecto =[
@@ -19,31 +20,49 @@ import img1 from './img/img inicio.png';
 // localStorage.setItem("tareas_v1", JSON.stringify(tareasDefecto))
 
 function useLocalStorage (itemName, initialValue){
+
+  const[item , setItem] = React.useState(initialValue)
+  const[loading , setLoaging] = React.useState(true)
+  const[error , setError] = React.useState(false)
   
-  const localStorageItem = localStorage.getItem(itemName) // inicializar datos en local storage convertiendo string a array
-  let parsedItem 
 
-  if(!localStorageItem){  // condicion para determinar si se tiene infomacion en el local storage
-    localStorage.setItem(itemName, JSON.stringify(initialValue)) // si no se tiene info genera por defecto in array vacio
-    parsedItem =initialValue
-  }else{
-    parsedItem = JSON.parse(localStorageItem) // si tiene info se utiliza PARSE para visualizarla en el navegador
-  }
+  React.useEffect(() =>{
+  setTimeout(() =>{
+    try{
+      const localStorageItem = localStorage.getItem(itemName) // inicializar datos en local storage convertiendo string a array
+      let parsedItem 
+  
+      if(!localStorageItem){  // condicion para determinar si se tiene infomacion en el local storage
+        localStorage.setItem(itemName, JSON.stringify(initialValue)) // si no se tiene info genera por defecto in array vacio
+        parsedItem =initialValue
+      }else{
+      parsedItem = JSON.parse(localStorageItem) // si tiene info se utiliza PARSE para visualizarla en el navegador
+      setItem(parsedItem)
+      }
+  
+      setLoaging(false) // una vez cargue los datos iniales se cambia el estador de loging
+  
+    }catch(error){
+      setLoaging(false)
+      setError(true)  
+    }
 
-  const[item , setItem] = React.useState(parsedItem)
+  }, 2000)
+ 
+}, [])
 
   const guardarItem =(newitem) =>{  // funcion para guardar cambios en el local Storage
     localStorage.setItem("tareas_v1", JSON.stringify(newitem)) // actualizar y guardar tareas en el LOCAL STORAGE
     setItem(newitem) // enviar tareas actuales
   }
 
-  return [item, guardarItem]
+  return [item, guardarItem, loading, error]
 
 }
 
 function App() {
 
-  const[contandorTareas,setcontandoTareas]= useLocalStorage("tareas_v1" , []) // contador tareas
+  const[contandorTareas,setcontandoTareas,loading,error]= useLocalStorage("tareas_v1" , []) // contador tareas
   const[textoBuscador,settextoBuscador]= React.useState("") // lee estados-eventos, recibe lo escrito por el usuario
 
   const tareasCompletas = contandorTareas.filter(tarea => tarea.completed).length // contar numero de tareas existentes
@@ -84,6 +103,10 @@ function App() {
       />
     <div className ="container"> 
         <ComponenteLista>
+          <div className='efectosCargar'>
+            {loading &&  <img className='loading' src={imgLogo} alt="LOADING" />}
+            {error && <h1> Error indesperado!! </h1>}
+          </div>
           {buscarTareas.map(todo=>(
             <Tareas
               key ={todo.text}
